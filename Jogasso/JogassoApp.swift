2e6200524e6c032b992ba4a07358e9d44af90b05
domiuaunoob@outlook.com <domiuau>
2024-10-07 16:20:08 -0300

    Initial Commit

---
+++ Jogasso/JogassoApp.swift
@@ -0,0 +1,17 @@
+//
+//  JogassoApp.swift
+//  Jogasso
+//
+//  Created by GUILHERME MATEUS SOUSA SANTOS on 07/10/24.
+//
+
+import SwiftUI
+
+@main
+struct JogassoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
