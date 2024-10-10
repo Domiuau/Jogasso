941dc46c2db38932ffe1cbe900867f3f3be2f669
José Elias <joseelias5589@gmail.com>
2024-10-10 14:18:39 -0300

    JoseView

---
+++ Jogasso/JoseView.swift
@@ -0,0 +1,18 @@
+//
+//  JoseView.swift
+//  Jogasso
+//
+//  Created by JOSE ELIAS GOMES CAMARGO on 10/10/24.
+//
+
+import SwiftUI
+
+struct JoseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JoseView()
+}
