63691c94453336be0e4a0f64fee31e7409cb7103
pedro <pedro.ppsilva00@gmail.com>
2024-10-10 17:58:17 -0300

    Iniciando factory do tanque

--- Jogasso/ViewModel/InventarioViewModel.swift
+++ Jogasso/ViewModel/InventarioViewModel.swift
@@ -8,4 +8,22 @@
 import Foundation
 
 class InventarioViewModel {
+    
+    static func criarTanque(tipotanque: Int) -> Tanque?{
+        switch tipotanque {
+        case 1:
+            return Tanque(nome: "Tanque de Polietileno", descricao: "As caixas D’água são uma ótima escolha para iniciantes na piscicultira pelos seguintes motivos:", preco: 200, capacidade: 1000, funcao: "Comportar a agua e os peixes")
+            
+        case 2:
+            return Tanque(nome: "Tanque de Polietileno maior", descricao: "As caixas D’água são uma ótima escolha para iniciantes na piscicultira porem agora que sua piscicultura esta maior voce precisa :", preco: 200, capacidade: 1000, funcao: "Comportar a agua e os peixes")
+        case 3:
+            return Tanque(nome: "Tanque de Geomenbrana PVC", descricao: "Os Tanques de Geomembrana PVC são excelentes para quem precisa de  tamanhos e formatos de tanques personalizado.", preco: 700, capacidade: 5000, funcao: "Comportar a agua e os peixes")
+        case 4:
+            return Tanque(nome: "Tanque de Placas Cimentícias", descricao: "Os Tanques de Placas Cimenticias são ideais para quem deseja ter a estrutura mais robusta e duravel possível.", preco: 1500, capacidade: 8000, funcao: "Comportar a agua e os peixes")
+        default:
+            return nil
+        }
+        
+    }
+    
 }
