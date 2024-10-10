873333039c31e6208e709a3c7cbeddc52c840bff
José Elias <joseelias5589@gmail.com>
2024-10-10 14:20:19 -0300

    Jose

--- Jogasso/JoseView.swift
+++ Jogasso/JoseView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct JoseView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("joze")
+        Image(systemName: "square.and.arrow.up.fill")
+
     }
 }
 
