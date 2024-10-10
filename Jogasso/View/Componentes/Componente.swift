3e973f5ba8280aaeab5cb40ccf1f16d8ee20d3a3
José Elias <joseelias5589@gmail.com>
2024-10-10 15:41:10 -0300

    Adicionando Assets

---
+++ Jogasso/View/Componentes/Componente.swift
@@ -0,0 +1,18 @@
+//
+//  Componente.swift
+//  Jogasso
+//
+//  Created by JOSE ELIAS GOMES CAMARGO on 10/10/24.
+//
+
+import SwiftUI
+
+struct Componente: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Componente()
+}
