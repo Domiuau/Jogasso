56adb0a3f6299f36eaf4f68621d0a202aa140329
domiuaunoob@outlook.com <domiuau>
2024-10-10 16:54:40 -0300

    viewmodels

---
+++ Jogasso/ViewModel/InventarioViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  InventarioViewModel.swift
+//  Jogasso
+//
+//  Created by GUILHERME MATEUS SOUSA SANTOS on 10/10/24.
+//
+
+import Foundation
+
+class InventarioViewModel {
+}
---
+++ Jogasso/ViewModel/TanqueAtivoViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TanqueAtivoViewModel.swift
+//  Jogasso
+//
+//  Created by GUILHERME MATEUS SOUSA SANTOS on 10/10/24.
+//
+
+import Foundation
+
+class TanqueAtivoViewModel {
+    
+}
