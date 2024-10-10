e1ab0572ffa931ffbfa4de3eb91165f9ec2d4e1b
pedro <pedro.ppsilva00@gmail.com>
2024-10-10 16:03:25 -0300

    a

---
+++ Jogasso/Model/Entitites/Decantador.swift
@@ -0,0 +1,14 @@
+//
+//  Decantador.swift
+//  Jogasso
+//
+//  Created by PEDRO PAULO DA SILVA on 10/10/24.
+//
+
+import Foundation
+
+class Decantador: Item {
+    var nome: String
+    var descricao: String
+    var funcao: String
+}
---
+++ Jogasso/Model/Entitites/Sistema.swift
@@ -0,0 +1,15 @@
+//
+//  Sistema.swift
+//  Jogasso
+//
+//  Created by PEDRO PAULO DA SILVA on 10/10/24.
+//
+
+import Foundation
+
+class Sistema: Item {
+    
+    var decantador: Decantador
+    var filtro: Filtro
+    var funcao: String
+}
