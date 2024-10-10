b951089c88441d402aa9dadde4c3b3400a895072
domiuaunoob@outlook.com <domiuau>
2024-10-10 14:17:56 -0300

    test 2 git

--- Jogasso/ContentView.swift
+++ Jogasso/ContentView.swift
@@ -15,6 +15,7 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Hello, world!")
             Text("adsdsasad")
+            Text("TEXTO2")
         }
         .padding()
     }
